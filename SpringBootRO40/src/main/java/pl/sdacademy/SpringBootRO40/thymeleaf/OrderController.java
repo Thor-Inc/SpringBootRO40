8d4510ce72490f7a4db15da467b70da471bc77dc
CrinaT <crina_torous@yahoo.com>
2022-10-13 19:06:04 +0300

    L5: add hibernate validation error message

--- SpringBootRO40/src/main/java/pl/sdacademy/SpringBootRO40/thymeleaf/OrderController.java
+++ SpringBootRO40/src/main/java/pl/sdacademy/SpringBootRO40/thymeleaf/OrderController.java
@@ -2,10 +2,13 @@ package pl.sdacademy.SpringBootRO40.thymeleaf;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping("/orders")
@@ -22,14 +25,24 @@ public class OrderController {
     }
 
     @PostMapping("/create")
-    public String handleNewOrder(@ModelAttribute("orderForm") final OrderForm orderForm) {
+    public String handleNewOrder(@Valid @ModelAttribute("orderForm") final OrderForm orderForm, final Errors errors) {
+        if (errors.hasErrors()) {
+            return "orders";
+
+        /*
+        Metoda de validare alternativa
         if(validate(orderForm.getAmount())){
             return "redirect:/error";
         }
+        */
+            // orderForm fields are filled with user values
+            // handle new Order here
 
-        // orderForm fields are filled with user values
-        // handle new Order here
+
+
+        }
         return "redirect:/hello";
+
     }
     private boolean validate(int amount){
         return amount < 5;
